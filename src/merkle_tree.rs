9bf9806aef9e02cc7512efdf0b6fe201318d1d1a
ppoliani <ppoliani@gmail.com>
2022-02-16 18:22:37 +0200

    feat(*): add verify merkle path

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -30,4 +30,22 @@ impl MerkleTree {
   pub fn root_hex(&self) -> Option<String> {
     self.tree.root_hex()
   }
+
+  /// Note this is the exact same logic that will be used in the on-chain program as well
+  /// this is why we do not use the verify function from the underlying lib (rs_merkle)
+  pub fn verify(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
+    let mut computed_hash = leaf;
+
+    for proof_element in proof.into_iter() {
+      if computed_hash <= proof_element {
+        // Hash(current computed hash + current element of the proof)
+        computed_hash = hashv(&[&computed_hash, &proof_element]).0;
+      } else {
+        // Hash(current element of the proof + current computed hash)
+        computed_hash = hashv(&[&proof_element, &computed_hash]).0;
+      }
+    }
+    // Check if the computed hash (root) is equal to the provided root
+    computed_hash == root
+  }
 }
