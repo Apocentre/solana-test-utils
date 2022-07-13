982a9d0b325c2aa2a29d23176ebb043bf4526677
ppoliani <ppoliani@gmail.com>
2022-07-13 21:33:55 +0300

    refactor: remove async from deser zero account

--- src/serialization.rs
+++ src/serialization.rs
@@ -4,7 +4,7 @@ use bytemuck::{
   Pod,
 };
 
-pub async fn deser_zero_account<'a, T: Pod>(data: &'a [u8],) -> &'a T {
+pub fn deser_zero_account<'a, T: Pod>(data: &'a [u8],) -> &'a T {
   let data = &data[8..size_of::<T>() + 8];
   
   from_bytes::<T>(&data)
